16575c6d4b2b3f0b709195efffe14204ff95e860
Preethi Varsha <mpreethivarsha@gmail.com>
2021-11-23 22:26:06 +0530

    lines of code

--- CS20B030_AirLine_ReservationSystem/src/Flight.java
+++ CS20B030_AirLine_ReservationSystem/src/Flight.java
@@ -1,3 +1,6 @@
 public class Flight {
+    private static String to="",from="",arrival="",departure="",flightnum="";
+    private int fare=3000,jduration=60,numbclass=10;
+    private boolean bclass=false;
     
 }
